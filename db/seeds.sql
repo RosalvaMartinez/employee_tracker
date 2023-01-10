a38e02a6447b4c4ce5b849d21ece5da683d5ab0f
Rosalva Hazel Martinez <rosalvahmartinez@gmail.com>
2023-01-10 03:08:34 -0600

    Add seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,16 +9,16 @@ SELECT * FROM department;
 
 INSERT INTO roles (id, title, salary, department_id)
 VALUES
-    ( 1, "Sales Person", 70,000, 3),
-    ( 2, "Account Manager", 89,000, 1),
-    ( 3, "Lead Engineer", 120,000, 4);
+    ( 1, "Sales Person", 70000, 3),
+    ( 2, "Account Manager", 89000, 1),
+    ( 3, "Lead Engineer", 120000, 4);
 
 SELECT * FROM roles;
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES
-    ( 1, "Goose", "Longneck", 3),
-    ( 2, "Ducki", "Longbottom", 2),
-    ( 3, "Whiskey", "Peets", 1);
+    ( 1, "Goose", "Longneck", 3, 1),
+    ( 2, "Ducki", "Longbottom", 2, 2),
+    ( 3, "Whiskey", "Peets", 1, 3);
 
 SELECT * FROM employee;
--- index.js
+++ index.js
@@ -1,69 +1,113 @@
+const inquirer = require('inquirer')
+const Employee = require('./lib/Employee');
+const Department = require('./lib/Department');
+const Roles = require('./lib/Roles');
+const mysql = require('mysql2');
+
+
 function init() {
-    
-}
+    // create the connection to database
+    const db = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'employeelist_db',
+        password: 'whiskey1!'
+    });
+
+     emp = new Employee(67, results.firstName, results.last_name, 33, 3)
+    db.query(
+        `INSERT INTO employee VALUES(${emp.getId()}, '${emp.getFirstName()}', '${emp.getLastName()}', ${emp.getRoleId()}, ${emp.getManagerId()})`,
+        function (err, results, fields) {
+            console.log(results); // results contains rows returned by server
+        }
+    )
+    db.query(
+        `SELECTconst * FROM employee`,
+        function (err, results, fields) {
+            console.log(results); // results contains rows returned by server
+        }
+    );
+
+    emp = new Department(67, results.department_name)
+    db.query(
+        `INSERT INTO department VALUES(${emp.getId()}, '${emp.getDepartmentName()}')`,
+        function (err, results, fields) {
+            console.log(results); // results contains rows returned by server
+        }
+    )
+    db.query(
+        `SELECTconst * FROM department`,
+        function (err, results, fields) {
+            console.log(results); // results contains rows returned by server
+        }
+    );
+};
 
 
-What would you like to do? 
+// What would you like to do? 
 
->View All Employees
->Add Employee
->Update Employee Role
+// >View All Employees
+// >Add Employee
+// >Update Employee Role
 
->View All Roles
->Add Role
+// >View All Roles
+// >Add Role
 
->View All Departments
->Add Department
+// >View All Departments
+// >Add Department
 
->quit
+// >quit
 
 function viewALLEmployees() {
-//present employee table
+    //present employee table
 };
 
 function addEmployee() {
-//PROMPT: Enter employees first name
-//input 
-//PROMPT: Enter employees last name
-//input
-//PROMPT: Enter employees role
-//querry db for role options(roles table.title)
-//PROMPT: Enter employees manager
-//querry db for manager name options(employee table.title.manager)
-//auto create Employee ID
-//add employee to employee table 
+    //PROMPT: Enter employees first name
+    //input 
+    //PROMPT: Enter employees last name
+    //input
+    //PROMPT: Enter employees role
+    //querry db for role options(roles table.title)
+    //PROMPT: Enter employees manager
+    //querry db for manager name options(employee table.title.manager)
+    //auto create Employee ID
+    //add employee to employee table 
 };
 
 function updateEmployeeRole() {
-//PROMPT: Enter name of Employee
-//input
-//POMPT: Select Role
-//querry db for role options(roles table.title)
-//update employee table 
+    //PROMPT: Enter name of Employee
+    //input
+    //POMPT: Select Role
+    //querry db for role options(roles table.title)
+    //update employee table 
 };
 
 function viewAllRoles() {
-//present roles table
+    //present roles table
 };
 
 function addRole() {
-//PROMPT: Enter name of role
-//input
-//PROMPT: Enter salary
-//input
-//PROMPT: Enter Department
-//querry db for Department options(departments table.department_name)
-//auto create role ID
-//add employee to role table 
+    //PROMPT: Enter name of role
+    //input
+    //PROMPT: Enter salary
+    //input
+    //PROMPT: Enter Department
+    //querry db for Department options(departments table.department_name)
+    //auto create role ID
+    //add employee to role table 
 };
 
 function viewAllDepartments() {
-//present department table
+    //present department table
 };
 
 function addDepartment() {
-//PROMPT: Enter name of Department
-//input
-//auto create department ID
-//save input to deoartment table
+    //PROMPT: Enter name of Department
+    //input
+    //auto create department ID
+    //save input to deoartment table
 };
+
+
+init();
